070a34f27d343488fe93a89dfaa370989b0c33a3
prabhk <prabhk@sfu.ca>
2020-07-22 20:56:17 -0700

    player can now attack and kill the monster

--- public/game/game.js
+++ public/game/game.js
@@ -24,8 +24,11 @@ var config = {
 };
 
 
-var controls
+var controls;
+var keySpace;
 var playerVelocityModifier = 0;
+var worldHeight;
+var worldWidth;
 
 function preload() {
     // adding array of character with 178 unique positions
@@ -33,31 +36,45 @@ function preload() {
     this.load.spritesheet('monsterCharacter', 'assets/monsterCharacter.png',{ frameWidth: 64, frameHeight: 64, endFrame: 272 });
     this.load.image('ground', 'assets/platform.png');
     this.load.image('powerupWater', 'assets/temp_bottle.png');
+    this.load.image('weapon', 'assets/axe.png');
 }
 
 function create() {
+    console.log(this);
     this.cameras.main.backgroundColor.setTo(61,72,73); // Set background colour using RGB
     var mainCharacterRows = 13;
 
     //Adding main character to game
-    var viking = this.physics.add.sprite(100, 450, 'mainCharacter', 6 * (mainCharacterRows) + (7)); // Initially Places the Viking 
+    var viking = this.physics.add.sprite(100, 500, 'mainCharacter', 6 * (mainCharacterRows) + (7)); // Initially Places the Viking 
     
     //Adding monster to game
     this.monster = this.physics.add.sprite(200, 200, 'monsterCharacter', 11 * (mainCharacterRows) + 0);
 
     //Adds the water bottle power up to the game.
-    var waterBottle = this.physics.add.sprite(100,400, 'powerupWater').setScale(0.2);
+    var waterBottle = this.physics.add.sprite(100,450, 'powerupWater').setScale(0.2);
     waterBottle.body.setAllowGravity(false);
     this.waterBottle = waterBottle;
+
+    this.axes = this.physics.add.group({
+        defaultKey: 'weapon',
+        maxSize: 3,
+    });
 
 
     var platforms = this.physics.add.staticGroup(); // Implments Physics
     this.physics.add.collider(viking, platforms);
     //Allows player to pick up water bottle power up
     var bottleCollider = this.physics.add.overlap(viking, waterBottle, powerUpWater.bind(this));
+    var monsterCollider = this.physics.add.overlap(this.axes, this.monster, destroyMonster.bind(this));
+    console.log(this.physics.world.bounds);
+    console.log(this.physics.world.bounds.height);
+    worldHeight = this.physics.world.bounds.height;
+    worldWidth = this.physics.world.bounds.width;
     this.physics.add.collider(this.monster, platforms);
     this.physics.add.collider(this.monster, viking);
     this.controls = this.input.keyboard.createCursorKeys(); // Allows the access of user arrow key presses
+    this.input.on('pointerdown', throwAxe, this);
+    this.physics.world.on('worldbounds', destroyWeapon);
 
 
     //PLATFORM NOTE:
@@ -114,12 +131,11 @@ function create() {
         repeat: -1
     });
 
-  this.monster.setVelocityX(100);
+    this.monster.setVelocityX(100);
     
     this.monster.anims.play('monsterRight', true);
     
     this.viking = viking;
-    
 }
 
 function update() {
@@ -158,6 +174,16 @@ function update() {
         this.monster.anims.play('monsterRight', true);
     }
 
+    //Despawn weapon if it's off the screen.
+    this.axes.children.each(function(axe) {
+        if(axe.active) {
+            if (axe.y < 0 || axe.y > worldHeight || axe.x < 0 || axe.x > worldWidth) {
+                console.log("boom");
+                axe.setActive(false);
+            }
+        }
+    });
+
 }
 
 //Destroys powerUp on pick up and adds attributes to player.
@@ -171,6 +197,36 @@ function powerUpWater() {
 //Resets the velocity modifier for the player
 function resetPlayerVelocity() {
     playerVelocityModifier = 0;
+}
+
+function throwAxe(pointer) { // Allows the acces of the space key to create a flying axe //TODO fix velocity and despawn if outside screen
+    var vikingX = viking.getWorldTransformMatrix(new Phaser.GameObjects.Components.TransformMatrix()).decomposeMatrix().translateX;
+    var vikingY = viking.getWorldTransformMatrix(new Phaser.GameObjects.Components.TransformMatrix()).decomposeMatrix().translateY;
+    var axe = this.axes.get(vikingX, vikingY);
+    if(axe) {
+    axe.body.setAllowGravity(false);
+    axe.setActive(true);
+    axe.setVisible(true);
+    this.physics.moveToObject(axe, pointer, 400);
+    console.log(viking.body.velocity);
+    //axe.setVelocity(viking.body.velocity);
+    }
+}
+
+function destroyWeapon(body) {
+    var weapon = body.gameObject;
+    console.log("boom");
+    console.log(weapon);
+    if(weapon) {
+    weapon.setActive(false);
+    weapon.setVisible(false);
+    //weapon.destroy();
+    console.log("gone");
+    }
+}
+
+function destroyMonster() {
+    this.monster.destroy();
 }
 
 var game = new Phaser.Game(config);
